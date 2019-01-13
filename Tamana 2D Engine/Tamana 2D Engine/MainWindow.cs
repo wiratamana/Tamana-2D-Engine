e59885c145841e0d455c0a6f1077912bf01c0c02
Wiratamana <wiratamana@icloud.com>
2019-01-13 14:32:16 +0900

    Initialize Project

---
+++ Tamana 2D Engine/Tamana 2D Engine/MainWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace TamanaEngine
+{
+    class MainWindow : GameWindow
+    {
+        public MainWindow()
+            : base(1280, // initial width
+        720, // initial height
+        GraphicsMode.Default,
+        "dreamstatecoding",  // initial title
+        GameWindowFlags.Default,
+        DisplayDevice.Default,
+        4, // OpenGL major version
+        0, // OpenGL minor version
+        GraphicsContextFlags.ForwardCompatible)
+        {
+
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            CursorVisible = true;
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            Title = $"(Vsync: {VSync}) FPS: {1f / e.Time:0}";
+
+            Color4 backColor;
+            backColor.A = 1.0f;
+            backColor.R = 0.1f;
+            backColor.G = 0.1f;
+            backColor.B = 0.3f;
+            GL.ClearColor(backColor);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            SwapBuffers();
+        }
+    }
+}
